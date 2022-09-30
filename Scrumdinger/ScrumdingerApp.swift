1458028672c9a51e30a1699ae33839d327b164d1
Alexandr Ogurtsov <myonlygamess@gmail.com>
2022-09-30 23:24:41 +0300

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by aleksanderogurtsov on 30.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
