a5bfd93eaac15a3e43724214d0e1774423edf23c
Alexandr Ogurtsov <myonlygamess@gmail.com>
2022-10-08 14:30:13 +0300

    Chapter 4. Passing data

---
+++ Scrumdinger/ScrumsView.swift
@@ -0,0 +1,38 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by aleksanderogurtsov on 01.10.2022.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    @Binding var scrums: [DailyScrum]
+    
+    var body: some View {
+        List {
+            ForEach($scrums, id: \.id) { $scrum in
+                NavigationLink(destination: DetailView(scrum: $scrum)) {
+                    CardView(scrum: scrum)
+                }
+                .listRowBackground(scrum.theme.mainColor)
+            }
+        }
+        .navigationTitle("Daily Scrums")
+        .toolbar {
+            Button(action: {}) {
+                Image(systemName: "plus")
+            }
+            .accessibilityLabel("New Scrum")
+        }
+    }
+}
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            ScrumsView(scrums: .constant(DailyScrum.sampleData))
+        }
+    }
+}
---
+++ Scrumdinger/TrailingIconLabelStyle.swift
@@ -0,0 +1,21 @@
+//
+//  TrailingIconLabelStyle.swift
+//  Scrumdinger
+//
+//  Created by aleksanderogurtsov on 01.10.2022.
+//
+
+import SwiftUI
+
+struct TrailingIconLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.title
+            configuration.icon
+        }
+    }
+}
+
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    static var trailingIcon: Self { Self() }
+}
