6eb3fe873a15a5310fbdcf1a486df9bdff2cab16
Alexandr Ogurtsov <myonlygamess@gmail.com>
2022-10-08 18:43:11 +0300

    Chapter 5. Section 2. Extract the meeting header

---
+++ Scrumdinger/MeetingHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  MeetingHeaderView.swift
+//  Scrumdinger
+//
+//  Created by aleksanderogurtsov on 08.10.2022.
+//
+
+import SwiftUI
+
+struct MeetingHeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MeetingHeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingHeaderView()
+    }
+}
